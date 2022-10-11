153ebd4b5c44268e5c2d96d9c787de450153d59d
tejas <tejasshah@gmail.com>
2022-10-11 08:03:09 +0530

    second

---
+++ break_continue.py
@@ -0,0 +1,24 @@
+'''
+for i in range(1,21):
+    if i%10 == 0:
+        break
+    print(" i:",i,end=" ")
+'''
+'''
+for i in range(1,21):
+    if i%2 == 0:
+        continue
+    print(" i:",i,end=" ")
+'''
+
+for i in [1,2,3]:
+    pass
+
+if 1 == 3:
+    pass
+
+if a > b:
+    pass
+else:
+    pass
+     
---
+++ while_loop.py
@@ -0,0 +1,22 @@
+i=1 #start 
+# 1 2 3 4 5 6*
+while i<=5: #condition 
+    print("royal => ",i)
+    i=i+1 #increment 
+print("out of the loop => ",i)
+
+
+
+i=10 
+
+while i >= 1:
+    print(i) #10 9 8 7 6 5 4 3 2 1 
+    i=i-1  
+
+
+i=1
+while i<=3:
+    print("i => ",i)
+    i=i+1
+else:
+    print(" End Of Loop")
